180a9238e456c4675b869cd3ab5657a5f74c055e
Mike Shaw <mike@applimatix.com>
2018-04-09 11:19:14 +1000

    Initial project commit

---
+++ app/src/main/java/com/applimatix/wrapperlayout/WrapperLayout.java
@@ -0,0 +1,104 @@
+package com.applimatix.wrapperlayout;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.FrameLayout;
+
+
+/**
+ * Created by mike on 28/07/15.
+ */
+public class WrapperLayout extends FrameLayout {
+
+    private static final String TAG = "WrapperLayout";
+
+    private int layoutWrapper;
+    private int parentId;
+
+    public WrapperLayout(Context context) {
+        super(context);
+        init(null);
+    }
+
+    public WrapperLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs);
+    }
+
+    public WrapperLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(attrs);
+    }
+
+    @TargetApi(21)
+    public WrapperLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(attrs);
+    }
+
+    private void init(AttributeSet attrs) {
+        //read the custom attributes
+
+        if (attrs == null) {
+            Log.wtf(TAG, "attrs not defined");
+            return;
+        }
+
+        final TypedArray styledAttributes = getContext().obtainStyledAttributes(attrs, R.styleable.WrapperLayout);
+
+        if (styledAttributes == null) {
+            Log.wtf(TAG, "styledAttributes not defined");
+            return;
+        }
+
+        layoutWrapper = styledAttributes.getResourceId(R.styleable.WrapperLayout_layoutWrapper, 0);
+        parentId = styledAttributes.getResourceId(R.styleable.WrapperLayout_parentId, 0);
+
+        styledAttributes.recycle();
+
+        if (layoutWrapper == 0 || parentId == 0) {
+            Log.wtf(TAG, "layoutWrapper or parentId not defined");
+            return;
+        }
+
+        getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+                    getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                } else {
+                    getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                }
+                wrapLayout();
+            }
+        });
+    }
+
+    private void wrapLayout() {
+        final FrameLayout content = getRootView().findViewById(android.R.id.content);
+        final ViewGroup wrapper = (ViewGroup) inflate(getContext(), layoutWrapper, content);
+
+        if (wrapper == null) {
+            Log.wtf(TAG, "Could not inflate layoutWrapper");
+            return;
+        }
+
+        final ViewGroup parent = wrapper.findViewById(parentId);
+
+        if (parent == null) {
+            Log.wtf(TAG, "Could not find parentId in layoutWrapper");
+            return;
+        }
+
+        content.removeView(this);
+        parent.addView(this);
+
+        parent.requestLayout();
+    }
+}
